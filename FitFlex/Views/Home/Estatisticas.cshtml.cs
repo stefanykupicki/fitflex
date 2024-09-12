fed13568546ac11b01b8c14ddff7053d9021460d
Stefany Kupicki <stefanygabrielekupicki@gmail.com>
2024-09-12 18:07:24 -0300

    Versão Inicial da Plataforma de Treinos FitFlex

---
+++ FitFlex/Views/Home/Estatisticas.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FitFlex.Views.Home
+{
+    public class EstatisticasModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ FitFlex/Views/Home/Perfil.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FitFlex.Views.Home
+{
+    public class PerfilModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
